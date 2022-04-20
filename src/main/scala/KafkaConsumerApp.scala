3c999ed21df9db6fa8300ae7707b4555be472b44
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-04-20 10:44:15 +0200

    ver. final

--- src/main/scala/KafkaConsumerApp.scala
+++ src/main/scala/KafkaConsumerApp.scala
@@ -1,29 +1,32 @@
-
+import org.apache.kafka.clients.consumer.ConsumerConfig
 
 object KafkaConsumerApp extends App {
 
-  import java.time.Duration
   import org.apache.kafka.clients.consumer.KafkaConsumer
-  import java.util
+
+  import java.time.Duration
   import java.util.Properties
 
   val props = new Properties()
-  props.put("bootstrap.servers", "localhost:9092")
-  props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-  props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-  props.put("group.id", "test")
-  props.put("enable.auto.commit", "true")
-  props.put("auto.commit.interval.ms", "1000")
+  props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put(ConsumerConfig.GROUP_ID_CONFIG, "test")
   val consumer = new KafkaConsumer[String, String](props)
 
-  consumer.subscribe(util.Arrays.asList("janusz"))
+  import scala.jdk.CollectionConverters._
+
+  consumer.subscribe(Seq("janusz").asJava)
+  //consumer.subscribe(util.Arrays.asList("janusz"))
 
   while ( {
     true
   }) {
-    val records = consumer.poll(Duration.ofMillis(100))
-      .forEach(record => {
-        println(s"offset = ${record.offset}, key = ${record.key}, value = ${record.value}")
-      })
+    val records = consumer.poll(Duration.ofMillis(100)) //.asScala       //foreach
+    records.forEach(record => {
+      println(s"offset = ${record.offset}, key = ${record.key}, value = ${record.value}")
+    })
   }
+  consumer.close()
 }
+
